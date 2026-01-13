574a587c46de796f5be36bd48ef5e3af993d202f
Yulia <yuliya.f.s@gmail.com>
2026-01-13 12:01:23 +0200

    add vercel backend url for cors

--- backend/app.ts
+++ backend/app.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000" || "https://gifts-plus.vercel.app",
     credentials: true,
   })
 );
