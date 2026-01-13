792eacaf36718bd3b07d24ef33a7c5ebbbacf828
Yulia <yuliya.f.s@gmail.com>
2026-01-13 13:40:07 +0200

    fix origin for front end

--- backend/app.ts
+++ backend/app.ts
@@ -16,7 +16,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000" || "https://gifts-plus.vercel.app",
+    //origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
